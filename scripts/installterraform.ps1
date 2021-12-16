7a2ed0a67651c7073c3d65ddb9659d3137b599d1
techguycode <hugage@hotmail.com>
2021-12-16 09:18:15 -0500

    Update installterraform.ps1

--- scripts/installterraform.ps1
+++ scripts/installterraform.ps1
@@ -1,29 +1,28 @@
 Function Install-Terraform
 {
     # Ensure to run the function with administrator privilege 
-    if (-not (New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
-    { Write-Host -ForegroundColor Red -Object "!!! Please run as Administrator !!!"; return }
+   if (-not (New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+   { Write-Host -ForegroundColor Red -Object "!!! Please run as Administrator !!!"; return }
     
-    # Terrafrom download Url
-    $Url = 'https://www.terraform.io/downloads.html'
- 
+    # Get Terraform Latest Version
+    $Url = 'https://checkpoint-api.hashicorp.com/v1/check/terraform'
+    $web = (Invoke-WebRequest -Uri $Url).Content | ConvertFrom-Json
+    $currentversion = $web[0].current_version
+
+
     # Local path to download the terraform zip file
     $DownloadPath = 'C:\Terraform\'
- 
     # Reg Key to set the persistent PATH 
     $RegPathKey = 'HKLM:\System\CurrentControlSet\Control\Session Manager\Environment'
  
     # Create the local folder if it doesn't exist
     if ((Test-Path -Path $DownloadPath) -eq $false) { $null = New-Item -Path $DownloadPath -ItemType Directory -Force }
  
-    # Download the Terraform exe in zip format
-    $Web = Invoke-WebRequest -Uri $Url
-    $FileInfo = $Web.Links | Where-Object href -match windows_amd64
-    $DownloadLink = $FileInfo.href
-    $FileName = Split-Path -Path $DownloadLink -Leaf
-    $DownloadFile = [string]::Concat( $DownloadPath, $FileName )
+    # Download the Terraform exe in zip format for amd_64
+    $DownloadLink = "https://releases.hashicorp.com/terraform/$($currentversion)/terraform_$($currentversion)_windows_amd64.zip"
+    $DownloadFile = "c:\Terraform\tf_$($currentversion).zip"
     Invoke-RestMethod -Method Get -Uri $DownloadLink -OutFile $DownloadFile
- 
+  
     # Extract & delete the zip file
     Expand-Archive -Path $DownloadFile -DestinationPath $DownloadPath -Force
     Remove-Item -Path $DownloadFile -Force
